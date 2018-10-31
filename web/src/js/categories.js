8a87fe4ac51bcc4c13855fa020ec06d88f2d32b8
Martin Nikl <m.nikl@seznam.cz>
2018-10-31 15:43:44 +0100

    active filters

--- web/src/js/categories.js
+++ web/src/js/categories.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
   const initSlider = () => {
     const defaultMinValue = 234
     const defaultMaxValue = 987
-
     const minField = $('.filters__range-min')
     const maxField = $('.filters__range-max')
 
@@ -23,8 +22,6 @@ $(document).ready(function() {
         const minVal = values[0]
         const maxVal = values[1]
 
-        console.log(minField)
-
         minField.html(`${minVal} Kč`)
         maxField.html(`${maxVal} Kč`)
       }
@@ -33,29 +30,116 @@ $(document).ready(function() {
 
   initSlider();
 
-  const filterSelect = $('#categories-filters')
+  const activeFilters = []
 
-  filterSelect.on('change', function() {
-    const val = $(this).val()
-    const cateogryLists = $('.filters__category')
+  window.categoryFilterSelection = () => {
+    const DEFAULT_FILTER_VALUE = 'default'
+    let currentFilterCategory = DEFAULT_FILTER_VALUE;
+    const filterSelect = $('#categories-filters')
+    const filterWrapper = $('.filters__select-category-filter__select-wrapper')
+    const categoryLists = $('.filters__category')
+    const closeButton = $('.filters__close-category')
 
-    if (val === 'default') {
-      cateogryLists.hide()
-      $('.filters__select-category-filter__select-wrapper').removeClass('filters__select-category-filter__select-wrapper--open')
+    filterSelect.on('change', filteringLogicFuntion)
 
-      return
+    filteringLogicFuntion()
+    handleOnClose()
+    handleFilterItemChange()
+
+    function filteringLogicFuntion() {
+      showHideActiveFilters()
+      
+      currentFilterCategory = filterSelect.val()
+
+      if (currentFilterCategory === DEFAULT_FILTER_VALUE) {
+        categoryLists.hide()
+        filterWrapper.removeClass('filters__select-category-filter__select-wrapper--open')
+
+        return
+      }
+
+      categoryLists.hide()
+
+      filterWrapper.addClass('filters__select-category-filter__select-wrapper--open')
+      $(`.filters__category--${currentFilterCategory}`).show()
     }
 
-    cateogryLists.hide()
+    function handleOnClose() {
+      closeButton.on('click', () => {
+        filterSelect.val(DEFAULT_FILTER_VALUE)
+        categoryLists.hide()
+        filterWrapper.removeClass('filters__select-category-filter__select-wrapper--open')
+      })
+    }
 
-    $('.filters__select-category-filter__select-wrapper').addClass('filters__select-category-filter__select-wrapper--open')
-    $(`.filters__category--${val}`).show()
+    function handleFilterItemChange() {
+      $('.filters__category .filters__category__item input[type=checkbox]').on('change', function() {
+        const filterName = $(this).attr('data-label') || $(this).attr('name')
+        const active = activeFilters.find(i => i === filterName) || false
 
-  })
+        if (active) {
+          removeActiveFilter(filterName)
+        } else {
+          activeFilters.push(filterName)
+        }
 
-  $('.filters__close-category').on('click', () => {
-    filterSelect.val('default')
-    $('.filters__category').hide()
-    $('.filters__select-category-filter__select-wrapper').removeClass('filters__select-category-filter__select-wrapper--open')
-  })
+        emptyActiveFilters()
+        renderActiveFilters()
+
+        showHideActiveFilters()
+      })
+    }
+
+    function removeActiveFilter(filterName) {
+      const itemIndex = activeFilters.indexOf(filterName)
+      activeFilters.splice(itemIndex, 1)
+    }
+
+    function emptyActiveFilters() {
+      $('.filters__active-filters__list-wrapper').empty()
+    }
+
+    function renderActiveFilters() {
+      const content = $('<ul/>', { class: 'filters__active-filters__list' })
+
+      for (let i = 0; i < activeFilters.length; i++) {
+        const listItem = $('<li class="filters__active-filters__item" />')
+
+        $(`
+          <button type="button" class="filters__active-filters__button" data-label=${activeFilters[i]}>
+            <span class="filters__active-filters__text">${activeFilters[i]}</span>
+            <i class="icon icon-cancel"></i>
+          </button>
+        `)
+        .on('click', function() {
+          const label = $(this).attr('data-label')
+
+          removeActiveFilter(label)
+          emptyActiveFilters()
+          renderActiveFilters()
+
+          $(`.filters__category`).find(`input[data-label=${label}]`).prop('checked', false)
+
+          showHideActiveFilters()
+        })
+        .appendTo(listItem)
+
+        listItem.appendTo(content)
+      }
+
+      $(content).appendTo($('.filters__active-filters__list-wrapper'))
+    }
+
+    function showHideActiveFilters() {
+      const filtersElement = $('.filters__active-filters')
+
+      if (activeFilters.length > 0) {
+        filtersElement.removeClass('filters__active-filters--hidden')
+      } else {
+        filtersElement.addClass('filters__active-filters--hidden')
+      }
+    }
+  }
+
+  window.categoryFilterSelection();
 })
